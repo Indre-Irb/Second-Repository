a1f7b29b7368fd4340f3196f01afedc786773eb7
unknown <indre.irbinskaite@gmail.com>
2021-11-29 12:24:04 +0200

    add some content

--- Uzduotis52-1/Uzduotis52-1.js
+++ Uzduotis52-1/Uzduotis52-1.js
@@ -1,1 +1,9 @@
 console.log("Second try")
+
+const button = document.querySelector(".button")
+const box = document.querySelector(".box")
+
+button.onclick = () => {
+    box.innerHTML += `<div>Geros dienos</div>`
+
+}
